7e0abc99479ee3c6a21144aa10d71ba23b15bead
Aqazde <adil64489@gmail.com>
2023-01-19 10:21:28 +0600

    edited

--- Assignment 1/Phone/Main.java
+++ Assignment 1/Phone/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        double[ ] list1 = {3.1, 3.1, 2.5, 6.4};
-        selectionSort(list1);
-        System.out.println(list1);
+        Phone mother = new Phone("+777799992211","Ipad pro", 250.3f);
+        Phone father = new Phone(" +491995969", "Iphone 12", 210);
+        Phone brother = new Phone(" +411519181", "Samsuns S10", 219.3f);
+        Phone me = new Phone("+4915159957622", "Toyota", 2341.3f);
+        mother.print();
+        father.print();
+        brother.print();
+        mother.receiveCall("Mama");
+        father.receiveCall("Papa");
+        brother.receiveCall("Son");
+
+        me.sendMessage("123", "213213");
     }
 }
